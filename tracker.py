2c68dbc61a1d32d5f903e7f99f5111ff339669f7
Ethan <ethanhk6@uci.edu>
2025-11-18 03:33:36 -0800

    Add JSON loading and basic list command

--- tracker.py
+++ tracker.py
@@ -1,4 +1,8 @@
+import json
+from pathlib import Path
 import sys
+
+DATA_FILE = Path("data/sessions.json") #Defines where my data file is and that is under data folder 
 
 def main():
     if len(sys.argv) < 2:
@@ -14,14 +18,35 @@ def main():
     elif command == "summary":
         handle_summary()
     else:
-        print("Unknown command. {command}")
-    
+        print(f"Unknown command. {command}")
+
+
+
+#This is how we read a .json file and then read the data from it. In another function or something
+# we can call this function to get the data from the file.
+def load_sessions():
+    """Load sessions from the data file."""
+    # TODO: Implement loading logic
+    with DATA_FILE.open("r", encoding="utf-8") as file: #Open the data file in read mode
+        data = json.load(file) #Read the JSON and turn into python object and store in data variable
+    return data
+
     
 def handle_add():
     print("TODO: Implement add functionality")
     
 def handle_list():
-    print("TODO: Implement list functionality")
+    sessions = load_sessions()
+    
+    if not sessions:
+        print("There are no sessions recorded.")
+        return
+    
+    for s in sessions:
+        course = s['course']
+        duration = s['duration']
+        date = s['date']
+        print(f"Course: {course}, Duration: {duration}, Date: {date}")
     
 def handle_summary():
     print("TODO: Implement summary functionality")
